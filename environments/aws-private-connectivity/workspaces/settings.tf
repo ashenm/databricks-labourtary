8c5ed34b44cd929bfef03b38841acbc688c5af1c
Ashen Gunaratne <mail@ashenm.dev>
2026-05-21 08:56:52 +0000

    [environments/aws-private-connectivity] Enable Genie agent mode

--- environments/aws-private-connectivity/workspaces/settings.tf
+++ environments/aws-private-connectivity/workspaces/settings.tf
@@ -2,6 +2,12 @@ resource "databricks_workspace_setting_v2" "llm_proxy_partner_powered" {
   name        = "llm_proxy_partner_powered"
   boolean_val = { value = true }
 }
+
+resource "databricks_workspace_setting_v2" "genie_deep_research" {
+  name        = "genie_deep_research"
+  boolean_val = { value = true }
+}
+
 resource "databricks_ip_access_list" "main" {
   label        = "DEFAULT"
   list_type    = "ALLOW"

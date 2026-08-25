7382a842dd376dbe434c18e3b5a46c8c252f76cb
Ashen Gunaratne <mail@ashenm.ml>
2026-08-25 16:01:14 +0000

    [environments/aws-fck-nat] Remove IP whitelist

--- environments/aws-private-connectivity/workspaces/settings.tf
+++ environments/aws-private-connectivity/workspaces/settings.tf
@@ -7,10 +7,3 @@ resource "databricks_workspace_setting_v2" "genie_deep_research" {
   name        = "genie_deep_research"
   boolean_val = { value = true }
 }
-
-resource "databricks_ip_access_list" "main" {
-  label        = "DEFAULT"
-  list_type    = "ALLOW"
-  enabled      = true
-  ip_addresses = ["130.41.0.0/16"]
-}
